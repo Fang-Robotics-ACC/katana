be6947a3d7aa864d7c3e85c41dba9640d227d4e2
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-02 17:34:29 -0600

    feat: begin part of color reduction tutorial

--- main.cpp
+++ main.cpp
@@ -1,25 +1,93 @@
+#include <opencv2/core/base.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/utility.hpp>
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <array>
+
+using Table256 = std::array<uint8_t, 256>;
+
+int reduceColor(int color, int spaceSize)
+{
+    const int kReducedIndex{color / spaceSize} ;
+    const int kRescaled{kReducedIndex * spaceSize};
+    return kRescaled;
+}
+
+Table256 generateColorReductionTable256(int spaceSize)
+{
+    Table256 reductionTable{};
+
+    for(int i{0}; i < 256; ++i)
+    {
+        reductionTable[i] = reduceColor(i, spaceSize);
+    }
+
+    return std::move(reductionTable);
+}
+
+cv::Mat& scanContinuousImageAndReduceColor(cv::Mat& I, const Table256& table)
+{
+
+    assert(I.isContinuous());
+    const int kChannels = I.channels();
+    const int kRows = I.rows;
+    const int kCols = I.cols * kChannels;
+
+    const int kColorValues{kRows * kCols};
+    for(int i{0}; i < kColorValues; ++i)
+    {
+        uint8_t *const kElementPtr{I.ptr(0)}; //Only one row
+        const uint8_t kElementValue{*kElementPtr};
+        kElementPtr[i] = table[kElementValue];
+        std::cout << kElementPtr[i];
+    }
+    return I;
+}
+
+cv::Mat& scanImageAndReduceColor(cv::Mat& I, const Table256& table)
+{
+    CV_Assert(I.depth() == CV_8U);
+
+    const int kChannels = I.channels();
+    const int kRows = I.rows;
+    const int kCols = I.cols * kChannels;
+
+    for(int row{0}; row < kRows; ++row)
+    {
+        uint8_t *const kRowArray{I.ptr(row)};
+        for(int column{0}; column < kCols; ++column)
+        {
+            const uint8_t kColor{kRowArray[column]};
+            kRowArray[column] = table[kRowArray[column]];
+        }
+    }
+    return I;
+}
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
+    // Verify arguments
+	if (argc != 3)
 	{
 		printf("usage: Displayimage");
 		return -1;
 	}
 
-	cv::Mat image{cv::imread(argv[1], cv::IMREAD_COLOR)};
+    // Generate reduction table
+    const int kSpaceSize{std::stoi(argv[2])};
+    const Table256 kReductionTable{
+        generateColorReductionTable256(kSpaceSize)
+    };
 
-	if (!image.data)
-	{
-		printf("No image data\n");
-		return -1;
-	}
-	cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
-	cv::imshow ("Display Image", image);
+    cv::Mat image{cv::imread(argv[1])};
+    cv::Mat image2{cv::imread(argv[1])};
 
-	cv::waitKey(0);
+    scanContinuousImageAndReduceColor(image,kReductionTable );
+
+    //cv::imshow("Yay", image);
+    cv::imshow("yay", image2);
 
 	return 0;
 }
