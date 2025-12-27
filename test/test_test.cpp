fadbc80ca5bd6bd02c0f988c5bd6d32ff9291dbd
RavenRandomz <raven.randomz.code@protonmail.com>
2025-12-26 18:12:11 -0600

    docs: Explain why this existed

--- test/test_test.cpp
+++ test/test_test.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 
+/**
+ * This was initially here just for the sake of prevent cmake from complaining
+ * from having empty sources
+ */
 TEST(test, unitTest)
 {
     EXPECT_TRUE(2340);
